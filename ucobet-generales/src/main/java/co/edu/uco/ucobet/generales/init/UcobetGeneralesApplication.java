3af69aaf8437d774569fb56b508cca7c354ba73a
David-alzate <alzatedavid0126@gmail.com>
2024-10-15 18:56:27 -0500

    Creacion de la configuracion y controller para los secretos del Key Vault usando Azure

--- ucobet-generales/src/main/java/co/edu/uco/ucobet/generales/init/UcobetGeneralesApplication.java
+++ ucobet-generales/src/main/java/co/edu/uco/ucobet/generales/init/UcobetGeneralesApplication.java
@@ -1,15 +1,36 @@
 package co.edu.uco.ucobet.generales.init;
 
+import com.azure.security.keyvault.secrets.SecretClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"co.edu.uco.ucobet.generales.infrastructure.primaryadapters.controller", "co.edu.uco.ucobet.generales.init.config" })
-public class UcobetGeneralesApplication {
+@ComponentScan(basePackages = {"co.edu.uco.ucobet.generales.infrastructure.primaryadapters.controller", 
+                               "co.edu.uco.ucobet.generales.init.config" })
+public class UcobetGeneralesApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UcobetGeneralesApplication.class, args);
-	}
+    private final SecretClient secretClient;
 
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    public UcobetGeneralesApplication(SecretClient secretClient) {
+        this.secretClient = secretClient;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcobetGeneralesApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        // Recupera el secreto desde Azure Key Vault y establece la URL de la base de datos
+        String h2url = secretClient.getSecret("spring-datasource-url").getValue();
+        System.setProperty("spring.datasource.url", h2url);
+        System.out.println("h2url: " + h2url);
+    }
 }
+
